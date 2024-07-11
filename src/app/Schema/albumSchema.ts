2ab56788bb1b93a0a0dc114b3a41abdc76bc0dd1
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-11 21:12:19 +0530

    .

--- src/app/Schema/albumSchema.ts
+++ src/app/Schema/albumSchema.ts
@@ -1,4 +1,3 @@
-"use client";
 import { z } from 'zod';
 
 export const albumSchema = z.object({
@@ -8,8 +7,10 @@ export const albumSchema = z.object({
   genre: z.string().nonempty("Genre is required"),
   label: z.string().nonempty("Record label is required"),
   language: z.string().nonempty("Language is required"),
-  //googleDriveLink: z.string().url("Must be a valid URL"),
   producer: z.string().nonempty("Producer name is required"),
   duration: z.string().nonempty("Duration is required"),
   artwork: z.any(),
 });
+
+// Define the type for the FormData based on the schema
+export type FormData = z.infer<typeof albumSchema>;
